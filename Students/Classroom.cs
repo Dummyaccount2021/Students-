2f3db1f63fbd7d06a0b3b19254200a9361ec6815
AlexAntoine <antoinealex1234@gmial.com>
2021-03-30 23:44:09 -0400

    initialzed all different types of students.

--- Students/Classroom.cs
+++ Students/Classroom.cs
@@ -6,15 +6,22 @@ namespace Students
 {
     class Classroom
     {
-        CurryStudent cs = new CurryStudent();
         IO io = new IO();
 
-        CurryStudent[] css = new CurryStudent[2];
+        CurryStudent [] csa = new CurryStudent[2];
+        DanceStudent [] dance = new DanceStudent[2];
+        JudoStudent [] judo = new JudoStudent[2];
+        NuseryStudent[] nurse = new NuseryStudent[2];
         public Classroom()
         {
-           
+            for (int a = 0; a < csa.Length; a++)
+            {
+                csa[a] = new CurryStudent();
 
+                Console.WriteLine("what is your first name?");
+                while (!csa[a].setFirstName(Console.ReadLine())) ;
 
+            }
         }
 
       
